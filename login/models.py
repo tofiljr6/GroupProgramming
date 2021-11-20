cf595ec69e8fc065c0ae84b85be575c7af1d213d
qsko <mi.zamelski@gmail.com>
2021-11-20 09:31:27 +0100

    dbcreation pt2

--- login/models.py
+++ login/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.db.models.deletion import SET_NULL
+from django.db.models.fields import DateField
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    point_umber =models.IntegerField(default=0)
+    role = models.TextChoices('Role', 'GUEST WAITER KITCHEN BAR MANAGER')
+
+class Table(models.Model):
+    isFree = models.TextChoices('isFree', 'FREE TAKEN RESERVED')
+
+class Table_Order(models.Model):
+    table_id = models.ForeignKey(Table, on_delete= models.SET_NULL, null= True)
+    user_id = models.ForeignKey(User, on_delete= models.SET_NULL, null= True, related_name='user')
+    waiter_id = models.ForeignKey(User, on_delete= models.SET_NULL, null= True, related_name='waiter')
+    is_paid = models.BooleanField(default= False)
+
+class Supply(models.Model):
+    product = models.CharField(max_length=200)
+    amount = models.FloatField()
+    date = models.DateField()
+    realised = models.BooleanField(default=False)
+
+
+
+class Menu(models.Model):
+    type = models.TextChoices('Type', 'DRINK DISH')
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+
+class Order(models.Model):
+    table_order_id = models.ForeignKey(Table_Order, on_delete= models.SET_NULL, null= True)
+    dish_id = models.ForeignKey(Menu, on_delete=SET_NULL, null= True)
+    where = models.TextChoices('Where', 'BAR KITCHEN')
+
+
+class History(models.Model):
+    user_id = models.ForeignKey(User, on_delete= models.SET_NULL, null= True)
+    order_id = models.ForeignKey(Order, on_delete= models.SET_NULL, null= True)
+
+
+
+    
