1f728c4374ec97f89b0a3d41f1f86fb3c82bbdbe
tofiljr6 <mateusz.tofil52@gmail.com>
2021-11-21 10:50:44 +0100

    sign up from with basic bootstrap

--- project/settings.py
+++ project/settings.py
@@ -41,7 +41,10 @@ INSTALLED_APPS = [
     'menu',
     'tables_layout',
     'order',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
